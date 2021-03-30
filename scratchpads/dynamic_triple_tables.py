f1dcdd6cefa96c70d5167c375351c25ccb321eed
devdave <devdave@ominian.net>
2021-03-30 10:20:47 -0600

    Initial experiment with triple views using database

---
+++ scratchpads/dynamic_triple_tables.py
@@ -0,0 +1,171 @@
+"""
+
+    Resources:
+        https://doc.qt.io/qtforpython/PySide6/QtCore/QAbstractItemModel.html
+        https://doc.qt.io/qtforpython/PySide6/QtCore/QAbstractTableModel.html
+        https://doc.qt.io/qtforpython/PySide6/QtWidgets/QMainWindow.html
+
+    Goal:
+        Experiment with composite view
+
+"""
+
+import sys
+import PySide2
+from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2.QtCore import Qt
+
+
+class BasicModel(QtCore.QAbstractTableModel):
+
+    def __init__(self, data):
+        super(BasicModel, self).__init__()  # TODO research why the author used super this way
+        self._data = data
+
+
+    def headerData(self, section, orientation, role):
+
+        if role == Qt.DisplayRole:
+            return f"Test {section}"
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            return self._data[index.row()][index.column()]
+
+        elif role == Qt.ToolTipRole:
+            return f"This is a tool tip for [{index.row()}][{index.column()}]"
+
+        else:
+            pass
+
+    def rowCount(self, index):
+        return len(self._data)
+
+    def columnCount(self, index):
+        return len(self._data[0])
+
+    # def flags(self, index):
+    #     return [Qt.ItemIsSelectable]
+
+
+class DynamicModel(QtCore.QAbstractTableModel):
+
+    def __init__(self, headers, initial_data):
+        super(DynamicModel, self).__init__()
+        self._headers = headers
+        self._data = initial_data
+
+
+    def headerData(self, section:int, orientation:PySide2.QtCore.Qt.Orientation, role:int=...):
+
+        if role == Qt.DisplayRole:
+            return self._headers[section]
+
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            return self._data[index.row()][index.column()]
+
+        else:
+            pass
+
+    def fetch_row(self, row):
+        return self._data[row]
+
+    def rowCount(self, index):
+        return len(self._data)
+
+    def columnCount(self, parent:PySide2.QtCore.QModelIndex=...) -> int:
+        return len(self._data[0])
+
+
+
+class TripleApp(PySide2.QtCore.QObject):
+
+
+    def artist_row_clicked(self, index: PySide2.QtCore.QModelIndex):
+        print(index.row(), index.column(), index.model().fetch_row(index.row()) )
+
+
+
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        # copy & pasted
+        data = [
+            [4, 9, 2],
+            [1, 0, 0],
+            [3, 5, 0],
+            [3, 3, 2],
+            [7, 8, 9],
+        ]
+
+        # our data
+        self.model1 = DynamicModel(["C1", "C two", "C 3"], data)
+        self.model2 = BasicModel(data)
+        self.model3 = BasicModel(data)
+
+        # our widgets
+        self.artist_table = QtWidgets.QTableView()
+        self.table2 = QtWidgets.QTableView()
+        divide = QtWidgets.QSplitter()
+        self.table3 = QtWidgets.QTableView()
+
+        # get rid of the vertical headers
+        for table in [self.artist_table, self.table2, self.table3]:
+            table.verticalHeader().hide()
+            table.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
+
+
+        # setup the data
+        self.artist_table.setModel(self.model1)
+        self.table2.setModel(self.model2)
+        self.table3.setModel(self.model3)
+
+        # layouts
+        # top body
+        self.top = QtWidgets.QHBoxLayout()
+        self.top.setAlignment(Qt.AlignLeft)
+        self.top.addWidget(self.artist_table, True, Qt.AlignLeft)
+        self.top.addWidget(self.table2, True, Qt.AlignLeft)
+
+        # main body
+        self.body = QtWidgets.QVBoxLayout()
+        self.body.addLayout(self.top)
+        self.body.setAlignment(Qt.AlignLeft)  # Ever get a feeling I really want this to be alligned left?
+        self.body.addWidget(divide)
+        self.body.addWidget(self.table3)
+
+
+        self.frame = QtWidgets.QFrame(self)
+        self.frame.setLayout(self.body)
+
+        self.setCentralWidget(self.frame)
+
+        self.setWindowTitle("Triple View")
+
+    def bindApp(self, app):
+
+
+        self.artist_table.clicked.connect(app.artist_row_clicked)
+        pass
+
+
+
+def main(argv):
+    app = QtWidgets.QApplication(argv)
+    window = MainWindow()
+    table_controller = TripleApp(app)
+
+    window.bindApp(table_controller)
+
+    window.show()
+    app.exec_()
+
+
+
+if __name__ == "__main__":
+    main(sys.argv)
