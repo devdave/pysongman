4ba33816dd536a0cd322281c532e7c0660178985
devdave <devdave@ominian.net>
2021-04-01 14:25:28 -0600

    Add current song marque

--- scratchpads/music_player_basic.py
+++ scratchpads/music_player_basic.py
@@ -52,20 +52,32 @@ class PlayerWindow(QtWidgets.QWidget):
         self.time_display = QtWidgets.QLabel("0:00")
         self.diagnostics = QtWidgets.QLabel("Place holder")
         self.spectagraph = QtWidgets.QLabel("Spectragraph")
-        self.info_dash = QtWidgets.QHBoxLayout()
-        self.info_dash.addWidget(self.time_display)
-        self.info_dash.addWidget(self.diagnostics)
-        self.info_dash.addWidget(self.spectagraph)
+        self.info_dash1 = QtWidgets.QHBoxLayout()
+        self.info_dash1.addWidget(self.time_display)
+        self.info_dash1.addWidget(self.diagnostics)
+        self.info_dash1.addWidget(self.spectagraph)
+
+
+        self.info_dash = QtWidgets.QVBoxLayout()
+        self.current_song = QtWidgets.QLabel("Artist - Title (12:34)")
+        self.info_dash.addLayout(self.info_dash1)
+        self.info_dash.addWidget(self.current_song)
+
+        self.info_dash.setObjectName("infoDashboard")
+
 
         #Playlist controls
         self.repeat_button = QtWidgets.QPushButton("L")
         self.random_button = QtWidgets.QPushButton("RA")
+
 
         self.playlist = QtWidgets.QVBoxLayout()
         for button in [self.repeat_button, self.random_button]:
             button.setMinimumWidth(3)
             button.setMaximumWidth(25)
             self.playlist.addWidget(button)
+
+        self.playlist.setObjectName("playListControls")
 
         #line1
         self.body_playlist_behavior = QtWidgets.QHBoxLayout()
@@ -99,28 +111,39 @@ class PlayerWindow(QtWidgets.QWidget):
         self.volume_slider = QtWidgets.QSlider(Qt.Horizontal)
 
         self.controls = QtWidgets.QHBoxLayout()
+        self.controls.setObjectName("controlBar")
+
         for button in [self.previous_btn, self.play_btn, self.pause_btn, self.stop_btn, self.next_btn, self.mute_btn]:
             button.setMinimumWidth(3)
             button.setMaximumWidth(25)
             self.controls.addWidget(button)
 
+
         self.controls.addWidget(self.volume_slider)
 
+        #Menu bar
+        self.menu_bar = QtWidgets.QMenuBar()
+
+        self.menu_file = QtWidgets.QMenu("&File")
+        self.menu_play = QtWidgets.QMenu("&Play")
+        self.menu_options = QtWidgets.QMenu("&Options")
+        self.menu_view = QtWidgets.QMenu("&View")
+        self.menu_help = QtWidgets.QMenu("&Help")
+
+        for menu in [self.menu_file, self.menu_play, self.menu_options, self.menu_view, self.menu_help]:
+            self.menu_bar.addMenu(menu)
 
         # Put it all together
         self.main_body = QtWidgets.QVBoxLayout()
+        self.main_body.layout().setMenuBar(self.menu_bar)
         self.main_body.addLayout(self.body_playlist_behavior)
         self.main_body.addLayout(self.status_and_views)
         self.main_body.addLayout(self.controls)
 
+
         self.setLayout(self.main_body)
-        self.setMinimumWidth(450)
-
-
-
-
-
-
+        self.setWindowTitle("PySongMan")
+        self.setMinimumWidth(350)
 
 
 
