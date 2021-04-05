a537a8fb8ce867190b0947663105290293567fd2
devdave <devdave@ominian.net>
2021-04-05 12:12:59 -0600

    Throwing a stack overflow at the moment but rough outline is in progress

---
+++ pysongman/__init__.py
@@ -0,0 +1,7 @@
+
+import pathlib
+
+HERE = pathlib.Path(__file__).parent.absolute()
+RSRC_DIR = HERE / "resources"
+ICON_DIR = RSRC_DIR / "icons"
+
---
+++ pysongman/__main__.py
@@ -0,0 +1,27 @@
+import sys
+import argparse
+import pathlib
+
+from PySide2 import QtCore
+from PySide2 import QtWidgets
+
+
+
+from .controllers.player import PlayerController
+
+
+def main(song_file):
+    player = PlayerController(song_file)
+
+    player.view.show()
+
+    app = QtWidgets.QApplication(sys.argv)
+
+    return sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("song_file", nargs="?", default=None)
+    args = parser.parse_args()
+
+    main(args.song_file)
