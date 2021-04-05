0a4b6125f1a37c515a84b2de4ff5e8f2132c32a7
devdave <devdave@ominian.net>
2021-04-05 13:32:39 -0600

    Attempt to ping down stackoverflow issue

---
+++ run.py
@@ -0,0 +1,28 @@
+import sys
+import argparse
+import pathlib
+
+from PySide2 import QtCore
+from PySide2 import QtWidgets
+
+
+
+# from .controllers.player import PlayerController
+from pysongman.controllers.player import PlayerController
+
+def main(song_file):
+    player = PlayerController(song_file)
+
+    player.view.show()
+    print("Player shown")
+
+    app = QtWidgets.QApplication()
+
+    return sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("song_file", nargs="?", default=None)
+    args = parser.parse_args()
+
+    main(args.song_file)
