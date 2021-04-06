1d1af4b0af8be9d20c520a5724ff3d9cc05c9f53
devdave <devdave@ominian.net>
2021-04-06 10:57:39 -0600

    autoplay music files provided via command line

--- pysongman/__main__.py
+++ pysongman/__main__.py
@@ -8,19 +8,22 @@ from PySide2 import QtWidgets
 from .controllers.player import PlayerController
 
 
-def main(song_file):
+def main(song_file = None):
 
     app = QtWidgets.QApplication(sys.argv)
 
     player = PlayerController()
     player.show()
 
+    if song_file is not None:
+        player.add_song(song_file)
 
     return sys.exit(app.exec_())
 
-if __name__ == '__main__':
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("song_file", nargs="?", default=None)
-    # args = parser.parse_args()
 
-    main(None)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("song_file", nargs="?", default=None)
+    args = parser.parse_args()
+
+    main(args.song_file)
