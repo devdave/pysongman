43f51762a3dcc48a0b34500c42b6097c280843db
devdave <devdave@ominian.net>
2021-04-06 07:06:04 -0600

    Back in business

--- pysongman/__main__.py
+++ pysongman/__main__.py
@@ -2,34 +2,24 @@ import sys
 import argparse
 import pathlib
 
-# from PySide2 import QtCore
-# from PySide2 import QtWidgets
 from PyQt5 import QtCore
 from PyQt5 import QtWidgets
-
-
 
 from .controllers.player import PlayerController
 
 
-
 def main(song_file):
-    # player = PlayerController(song_file)
-    #
-    # player.view.show()
 
-    from .views.player_window import PlayerWindow
-    view = PlayerWindow()
+    app = QtWidgets.QApplication(sys.argv)
 
-    print("Player shown")
-
-    app = QtWidgets.QApplication()
+    player = PlayerController()
+    player.view.show()
 
     return sys.exit(app.exec_())
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("song_file", nargs="?", default=None)
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("song_file", nargs="?", default=None)
+    # args = parser.parse_args()
 
-    main(args.song_file)
+    main(None)
--- pysongman/views/player_window.py
+++ pysongman/views/player_window.py
@@ -1,15 +1,7 @@
 import typing as T
 import pathlib
 
-# import PySide2
-# from PySide2.QtGui import QPixmap
-# from PySide2 import QtCore
-# from PySide2.QtCore import Qt
-# from PySide2 import QtWidgets
-
-# import PyQt5
-# from PyQt5.QtGui import QPixmap
-# from PyQt5 import QtCore
+import PyQt5
 from PyQt5.QtCore import Qt
 from PyQt5 import QtWidgets
 
@@ -19,8 +11,11 @@ class PlayerWindow(QtWidgets.QWidget):
 
     def __init__(self, *args, **kwargs):
         super(PlayerWindow, self).__init__()
-
+        self.icons = {}
         self.setupUI()
+
+    def load_icons(self):
+
 
     def setupUI(self):
         """
