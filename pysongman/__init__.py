d8c39206162c982839d2781a8595daf66a105b3b
devdave <devdave@ominian.net>
2021-04-13 15:36:26 -0600

    Prep using OS agnostic configuration/data dir.

--- pysongman/__init__.py
+++ pysongman/__init__.py
@@ -1,7 +1,16 @@
 
-import pathlib
+import os
+import sys
+from pathlib import Path
 
-HERE = pathlib.Path(__file__).parent.absolute()
+import appdirs
+
+HOME = None
+APP_NAME = "pysongman"
+APP_AUTHOR = "DJW"
+
+HOME = appdirs.user_data_dir(APP_NAME, APP_AUTHOR)
+HERE = Path(__file__).parent.absolute()
 RSRC_DIR = HERE / "resources"
 ICON_DIR = RSRC_DIR / "icons"
 CSS_DIR = RSRC_DIR / "css"
