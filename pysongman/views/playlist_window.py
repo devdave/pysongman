b010187d55fa9aa85d5a9c3ae8ac300e1f3d5c54
devdave <devdave@ominian.net>
2021-04-15 06:19:37 -0600

    Make columns fit to size of their contents

--- pysongman/views/playlist_window.py
+++ pysongman/views/playlist_window.py
@@ -37,11 +37,12 @@ class PlaylistWindow(QtWidgets.QWidget):
 
         self.table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
         self.table.verticalHeader().hide()
-        self.table.horizontalHeader().hide()
+        # self.table.horizontalHeader().hide()
         self.table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
         self.table.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
-        self.table.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        # self.table.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         self.table.resizeColumnsToContents()
+        self.table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
         # Ever get the feeling I REALLY want this to expand?
         durationDelegate = DurationAlignDelegate(self.table)
         self.table.setItemDelegateForColumn(1, durationDelegate)
