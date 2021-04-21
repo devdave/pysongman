024f9da64ca9adadccfa37b746e210941bfdfe07
devdave <devdave@ominian.net>
2021-04-21 13:37:11 -0600

    Consolidate song tag logic into one class

---
+++ tests/test_lib_song_info_SongInfo.py
@@ -0,0 +1,7 @@
+"""
+    Testing is very difficult as my problem resources are copywritten songs
+     which is a problem as I don't like getting sued for file sharing.
+"""
+
+def test_handle_listing_correctly():
+    assert True
