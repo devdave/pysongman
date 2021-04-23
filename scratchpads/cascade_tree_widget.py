04ae6b9e40c5dc31de1d0261f1a85b98cf66be92
devdave <devdave@ominian.net>
2021-04-23 11:21:02 -0600

    To many changes to comment

--- scratchpads/cascade_tree_widget.py
+++ scratchpads/cascade_tree_widget.py
@@ -40,6 +40,92 @@ class ParentTableBridge(QtCore.QAbstractTableModel):
             else:
                 return record.path
 
+class MasterConfig(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(MasterConfig, self).__init__()
+        self.controllers = {}
+        self.folders = {}
+        self.subviews = {}
+
+
+        self.setupUI()
+
+        self.menu_tree.itemClicked.connect(self.on_treeitem_clicked)
+
+    def setupUI(self):
+
+        self.setWindowTitle("Master configuration")
+
+        self.left_side = QtWidgets.QVBoxLayout(self)
+        self.body_layout = QtWidgets.QHBoxLayout(self)
+
+
+        # left side
+        self.menu_tree = QtWidgets.QTreeWidget(self)
+        self.menu_tree.setMinimumHeight(325)
+        self.menu_tree.setMinimumWidth(180)
+
+        self.close_btn = QtWidgets.QPushButton("Close")
+        self.left_side.addWidget(self.menu_tree)
+        self.left_side.addWidget(self.close_btn)
+
+        # right side
+        self.mdi = QtWidgets.QMdiArea()
+
+        #put together
+        self.body_layout.addLayout(self.left_side)
+        self.body_layout.addWidget(self.mdi)
+
+        # shove it in a frame
+        self.frame = QtWidgets.QFrame()
+        self.frame.setLayout(self.body_layout)
+
+        self.setCentralWidget(self.frame)
+
+        self.buildTree()
+
+        self.setMinimumSize(600, 400)
+        self.setMaximumSize(600, 400)
+
+
+    def add_controller(self, controller_cls, identifier, label, group_id=None):
+        if group_id is None:
+            root = self.menu_tree
+        elif group_id in self.folders:
+            root = self.folders[group_id]
+        else:
+            raise Exception(f"{group_id} is not a valid folder id. - options are {self.folders.keys()}")
+
+        self.controllers[identifier] = controller_cls()
+        self.folders[identifier] = QtWidgets.QTreeWidgetItem(root, [label])
+        self.folders[identifier].setData(2, QtCore.Qt.EditRole, identifier)
+
+    def buildTree(self):
+        self.root_header = QtWidgets.QTreeWidgetItem(["Main config"])
+
+        self.menu_tree.setHeaderItem(self.root_header)
+
+    def on_treeitem_clicked(self, treeItem: QtWidgets.QTreeWidgetItem):
+        label = treeItem.text(0)
+        identifier = treeItem.text(2)
+        print(treeItem, label, identifier)
+        controller = self.controllers[identifier]
+
+
+        if identifier not in self.subviews:
+            sub = self.mdi.addSubWindow(controller.view)
+            self.subviews[identifier] = sub
+        else:
+            for subview in self.subviews.values():
+                subview.hide()
+
+        sub = self.subviews[identifier]
+
+        sub.setWindowFlag(Qt.FramelessWindowHint, True)
+        sub.showMaximized()
+        sub.show()
+
 
 
 class MasterConfigController(QtCore.QObject):
@@ -48,46 +134,67 @@ class MasterConfigController(QtCore.QObject):
         super(MasterConfigController, self).__init__()
         self.view = MasterConfig()
 
+        self.view.add_controller(MediaConfigController, "media_config", "Media Library")
+        self.view.add_controller(lambda : MediaConfigController("Second Config"), "media_config2", "Second config")
+
         self.subviews = {}
 
         self.connect()
 
     def connect(self):
-        self.view.menu_tree.itemClicked.connect(self.on_treeitem_clicked)
+        # self.view.menu_tree.itemClicked.connect(self.on_treeitem_clicked)
+        self.view.close_btn.clicked.connect(self.on_close_clicked)
+
+    def show(self):
+        self.view.show()
 
 
-    def on_treeitem_clicked(self, treeItem: QtWidgets.QTreeWidgetItem):
-        label = treeItem.text(0)
-        identifier = treeItem.text(2)
-        print(treeItem, label, identifier)
-        mconfig = MediaConfigWidget()
-        mconfig.setWindowTitle("Media Config")
-        if identifier not in self.subviews:
-            sub = self.view.mdi.addSubWindow(mconfig)
-            sub.setWindowFlag(Qt.FramelessWindowHint, True)
-            sub.showMaximized()
-            sub.show()
-            self.subviews[identifier] = sub
-        else:
-            sub = self.subviews[identifier]
-            sub.show()
-            sub.showMaximized()
+
+    def on_close_clicked(self):
+        self.view.close()
+
+
+class MediaConfigController(QtCore.QObject):
+    def __init__(self, optional_tile=None):
+        self.view = MediaConfigWidget(optional_tile=optional_tile)
+        self.make_connections()
+
+    def make_connections(self):
+        pass
+
+    def on_click_add_folder(self):
+        print("Add Folder")
+
+    def on_click_edit_folder(self):
+        print("Edit folder")
+
+    def on_click_remove_folder(self):
+        print("Remove Folder")
+
 
 
 class MediaConfigWidget(QtWidgets.QWidget):
 
-    def __init__(self):
+    def __init__(self, optional_tile=None):
         super(MediaConfigWidget, self).__init__()
+        self.optional_title = optional_tile
         self.setupUI()
 
     def setupUI(self):
 
-        self.setWindowTitle("Media configuration")
+        if self.optional_title is None:
+            self.setWindowTitle("Media configuration")
+        else:
+            self.setWindowTitle(self.optional_title)
 
         layout = QtWidgets.QGridLayout()
         self.setLayout(layout)
 
-        groupbox = QtWidgets.QGroupBox("Watch folder settings")
+        if self.optional_title is None:
+            groupbox = QtWidgets.QGroupBox("Watch folder settings")
+        else:
+            groupbox = QtWidgets.QGroupBox(self.optional_title)
+
         layout.addWidget(groupbox)
 
         vbox = QtWidgets.QVBoxLayout()
@@ -127,57 +234,7 @@ class MediaConfigWidget(QtWidgets.QWidget):
 
 
 
-class MasterConfig(QtWidgets.QMainWindow):
 
-    def __init__(self):
-        super(MasterConfig, self).__init__()
-        self.setupUI()
-
-    def setupUI(self):
-
-        self.setWindowTitle("Master configuration")
-
-        self.left_side = QtWidgets.QVBoxLayout(self)
-        self.body_layout = QtWidgets.QHBoxLayout(self)
-
-
-        # left side
-        self.menu_tree = QtWidgets.QTreeWidget(self)
-        self.menu_tree.setMinimumHeight(450)
-        self.menu_tree.setMinimumWidth(180)
-
-        self.close_btn = QtWidgets.QPushButton("Close")
-        self.left_side.addWidget(self.menu_tree)
-        self.left_side.addWidget(self.close_btn)
-
-        # right side
-        self.mdi = QtWidgets.QMdiArea()
-
-        #put together
-        self.body_layout.addLayout(self.left_side)
-        self.body_layout.addWidget(self.mdi)
-
-        # shove it in a frame
-        self.frame = QtWidgets.QFrame()
-        self.frame.setLayout(self.body_layout)
-
-        self.setCentralWidget(self.frame)
-
-        self.buildTree()
-
-        self.setMinimumSize(600, 400)
-        self.setMaximumSize(600, 400)
-
-
-
-
-    def buildTree(self):
-        self.root_header = QtWidgets.QTreeWidgetItem(["Main config"])
-        # root.setText(1, "Configuration")
-        self.menu_tree.setHeaderItem(self.root_header)
-
-        self.media_fldr = QtWidgets.QTreeWidgetItem(self.menu_tree, ["Media library"])
-        self.media_fldr.setData(2, QtCore.Qt.EditRole, "media_lib")
 
 
 
@@ -192,7 +249,7 @@ def main():
     app = QtWidgets.QApplication(sys.argv)
 
     controller = MasterConfigController()
-    controller.view.show()
+    controller.show()
 
     return app.exec_()
 
