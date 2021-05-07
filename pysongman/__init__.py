591f8d66c3ea5a2e789a5b433f260b7015ba9267
devdave <devdave@ominian.net>
2021-05-06 18:32:21 -0600

    Application absolute configuration/state flags and values

---
+++ pysongman/__init__.py
@@ -0,0 +1,17 @@
+import appdirs
+from pathlib import Path
+
+
+HERE = Path(__file__).parent
+__version__ = "0.0.1"
+APP_NAME = "pysongman"
+APP_AUTHOR = "DevDave"
+HOME = Path(appdirs.user_data_dir(APP_NAME, APP_AUTHOR))
+DB_FILE = HOME / f"{APP_NAME}.sqlite3"
+CONFIGURED_FLAG = HOME / "installed.txt"
+RSRC_DIR = HERE / "resources"
+ICON_DIR = RSRC_DIR / "icons"
+CSS_DIR = RSRC_DIR / "css"
+
+if HOME.exists() is False:
+    HOME.mkdir(parents=True, exist_ok=True)
