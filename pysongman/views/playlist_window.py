e4d0ed3482d28ca2fe9616f98fc6fce77344b0c0
devdave <devdave@ominian.net>
2021-05-09 13:19:11 -0600

    make playlist table look how I want it

--- pysongman/views/playlist_window.py
+++ pysongman/views/playlist_window.py
@@ -39,18 +39,19 @@ class PlaylistWindow(QtWidgets.QWidget):
         self.body = QtWidgets.QVBoxLayout(self)
 
         self.table = QtWidgets.QTableView()
+        self.table.setModel(self.model)
 
-        self.table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
         self.table.verticalHeader().hide()
 
-        self.table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+        # self.table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
         self.table.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
 
         self.table.resizeColumnsToContents()
+        self.table.hideColumn(0)
         self.table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
-        # Ever get the feeling I REALLY want this to expand?
+
         alignleft = AlignLeftDelegate(self.table)
-        self.table.setItemDelegateForColumn(1, alignleft)
+        self.table.setItemDelegateForColumn(2, alignleft)
 
 
         self.body.addWidget(self.table)
