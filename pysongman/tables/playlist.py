8d4356f167563bdd020134fd7f50d74ebead57aa
devdave <devdave@ominian.net>
2021-05-12 08:21:38 -0600

    wire in song(s) added slots

--- pysongman/tables/playlist.py
+++ pysongman/tables/playlist.py
@@ -1,3 +1,4 @@
+import logging
 import typing
 
 from .. import USE_PYSIDE
@@ -5,16 +6,24 @@ from .. import USE_PYSIDE
 if USE_PYSIDE:
     from PySide2 import QtCore
 
+    from pybass3.pys2_playlist import Pys2Playlist
+
     Qt = QtCore.Qt
+
+
+log = logging.getLogger(__name__)
 
 
 class PlaylistTableModel(QtCore.QAbstractTableModel):
 
-    def __init__(self, playlist):
+    playlist: Pys2Playlist
+
+    def __init__(self, playlist: Pys2Playlist):
         super(PlaylistTableModel, self).__init__()
         self.playlist = playlist
 
-        self.playlist.song_added.connect(self.song_added)
+        self.playlist.song_added.connect(self.on_song_added)
+        self.playlist.songs_added.connect(self.on_songs_added)
 
     def rowCount(self, parent: QtCore.QModelIndex = ...) -> int:
         return len(self.playlist)
@@ -53,9 +62,18 @@ class PlaylistTableModel(QtCore.QAbstractTableModel):
             if col == 2:
                 return song.file_path.as_posix()
 
-    def song_added(self, song_id: str):
-
+    def on_song_added(self, song_id: str):
+        log.debug("Telling PL Table a new song has been added.")
         index = self.playlist.get_indexof_song_by_id(song_id)
         index_model = QtCore.QModelIndex()
         self.beginInsertRows(index_model, index, index)
+        self.endInsertRows()
+
+    def on_songs_added(self, payload):
+        log.debug("Telling PL that multiple songs have been added.")
+        starting_index, song_ids = payload
+        index_model = QtCore.QModelIndex()
+
+        self.beginInsertRows(index_model, starting_index+1, len(song_ids))
+
         self.endInsertRows()
