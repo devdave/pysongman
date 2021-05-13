7b61685866060093eedcd535eb5b04f1c21c1c85
devdave <devdave@ominian.net>
2021-05-13 13:05:41 -0600

    Search window logic added

---
+++ pysongman/controllers/search.py
@@ -0,0 +1,98 @@
+from .. import USE_PYSIDE
+
+import logging
+
+
+if USE_PYSIDE is True:
+    from PySide2 import QtCore, QtGui
+    from PySide2.QtCore import Qt
+
+    from pybass3.pys2_playlist import Pys2Playlist
+
+from ..views.search_window import SearchWindow
+from ..lib.search_proxy import SearchFilterProxy
+
+log = logging.getLogger(__name__)
+
+class SearchController(QtCore.QObject):
+
+    pl_obj: Pys2Playlist
+
+    def __init__(self, playlist_table, playlist_obj):
+        super(SearchController, self).__init__()
+        self.pl_table = playlist_table
+        self.pl_obj = playlist_obj
+
+        self.view = SearchWindow()
+
+        self.proxy_model = SearchFilterProxy(self.view)
+        self.proxy_model.setSourceModel(self.pl_table)
+
+        self.view.results.setModel(self.proxy_model)
+        self.setup()
+
+    def setup(self):
+
+        self.view.results.setModel(self.proxy_model)
+        self.view.results.setModelColumn(3)
+
+        self.setup_connections()
+
+    def setup_connections(self):
+        self.view.search.textChanged.connect(self.on_search_edited)
+
+        self.view.search.returnPressed.connect(self.on_search_returned)
+
+        self.view.results.clicked.connect(lambda : log.debug("Clicked"))
+        self.view.results.doubleClicked.connect(self.on_search_double_clicked)
+
+    def setup_shortcuts(self):
+        pass
+
+
+
+    def on_search_double_clicked(self):
+        selected = self.view.results.selectionModel().selection().value(0).indexes()[0]
+        row = selected.row()
+        pid = self.proxy_model.index(row, 0)
+        data = self.proxy_model.itemData(pid)
+        song_id = data[Qt.DisplayRole]
+        self.pl_obj.play_song_by_id(song_id)
+        self.view.search.setText("")
+        self.view.hide()
+        self.proxy_model.setFilterString(None)
+        debug = 1
+
+
+
+    def on_search_edited(self, text):
+        self.proxy_model.setFilterString(text)
+
+    def on_search_returned(self):
+        log.debug("Play first entry if it exists")
+        if self.proxy_model.rowCount() > 0:
+            pid = self.proxy_model.index(0,0)
+            data = self.proxy_model.itemData(pid)
+            song_id = data[0]
+            log.debug("Play SID %s", song_id)
+            self.pl_obj.play_song_by_id(song_id)
+
+            self.view.search.setText("")
+            self.view.hide()
+            self.proxy_model.setFilterString(None)
+
+        else:
+            log.debug("Nothing to play")
+
+
+
+
+
+    def show(self):
+        self.view.show()
+
+    def hide(self):
+        self.view.hide()
+
+    def activate(self):
+        self.view.activateWindow()
--- pysongman/views/search_window.py
+++ pysongman/views/search_window.py
@@ -1,18 +1,54 @@
 from .. import USE_PYSIDE
 
 if USE_PYSIDE:
-    from PySide2 import QtCore, QtWidgets
-
-    from pybass3.pys2_playlist import Pys2Playlist
+    from PySide2 import QtCore, QtWidgets, QtGui
 
     Qt = QtCore.Qt
 
+
 class SearchWindow(QtWidgets.QWidget):
 
-    playlist: Pys2Playlist
-    search_box: QtWidgets.QTextEdit
-    choices: QtWidgets.QListView
+    key_search_down = QtCore.Signal()
+    key_search_up = QtCore.Signal()
+    selected = QtCore.Signal()
 
-    def __init__(self, playlist: Pys2Playlist):
+    search_box: QtWidgets.QTextEdit
+    choices: QtWidgets.QTableWidget
+
+
+    def __init__(self):
         super(SearchWindow, self).__init__()
-        self.playlist = playlist
+        self.setup_ui()
+        self.setup_shortcuts()
+
+    def setup_ui(self):
+
+        body = QtWidgets.QVBoxLayout(self)
+
+        self.search = QtWidgets.QLineEdit("")
+        body.addWidget(self.search)
+
+        self.results = QtWidgets.QListView()
+        body.addWidget(self.results)
+
+        self.search.installEventFilter(self)
+        self.search.setFocus()
+
+    def setup_shortcuts(self):
+
+        down_pressed = QtWidgets.QShortcut(QtGui.QKeySequence(Qt.Key_Enter), self.results)
+        down_pressed.activated.connect(lambda : self.selected.emit())
+
+
+    def eventFilter(self, watched:QtCore.QObject, event:QtCore.QEvent) -> bool:
+        if watched == self.search:
+            if event.type() == QtCore.QEvent.KeyPress:
+                if event.key() == Qt.Key_Down:
+                    self.key_search_down.emit()
+                    self.results.setFocus()
+                    return False
+
+                
+        return super(SearchWindow, self).eventFilter(watched, event)
+        
+        
