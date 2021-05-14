d22d092e11d7373a24afa8fed8596dc84e780a46
devdave <devdave@ominian.net>
2021-05-14 07:23:39 -0600

    Add in toolbar, type hints for toolbar, and remove min/max buttons from window bar

--- pysongman/views/media_window.py
+++ pysongman/views/media_window.py
@@ -5,22 +5,48 @@ from PySide2.QtCore import Qt
 from PySide2 import QtWidgets
 
 
-class Media(QtWidgets.QWidget):
+class MediaWindow(QtWidgets.QMainWindow):
+
+    artist_table: QtWidgets.QTableView
+    album_table: QtWidgets.QTableView
+    songs_table: QtWidgets.QTableView
+
+    toolbar: QtWidgets.QToolBar
+    add_button: QtWidgets.QPushButton
+    nuke_button: QtWidgets.QPushButton
+    add_menu: QtWidgets.QMenu
+    act_add_file: QtWidgets.QAction
+    act_add_dir: QtWidgets.QAction
+
 
     def __init__(self):
-        super(Media, self).__init__()
-
-        self.artist_table = None
-        self.album_table = None
-        self.songs_table = None
+        super(MediaWindow, self).__init__()
 
         self.top = None
         self.body = None
         self.frame = None
-        self.setupUI()
+        self.setup_ui()
+        self.setup_toolbar()
 
-    def setupUI(self):
+    def setup_toolbar(self):
+        self.toolbar = QtWidgets.QToolBar()
+
+        self.add_button = QtWidgets.QPushButton("Add...")
+        self.nuke_button = QtWidgets.QPushButton("Nuke")
+
+        self.addToolBar(Qt.BottomToolBarArea, self.toolbar)
+
+        self.toolbar.addWidget(self.add_button)
+        self.toolbar.addWidget(self.nuke_button)
+
+        self.add_menu = QtWidgets.QMenu()
+        self.act_add_file = QtWidgets.QAction("Add file(s)")
+        self.act_add_dir = QtWidgets.QAction("Add folder")
+        self.add_button.setMenu(self.add_menu)
+
+    def setup_ui(self):
         # our widgets
+        self.frame = QtWidgets.QFrame()
         self.artist_table = QtWidgets.QTableView()
         self.album_table = QtWidgets.QTableView()
         divide = QtWidgets.QSplitter()
@@ -30,6 +56,7 @@ class Media(QtWidgets.QWidget):
         for table in [self.artist_table, self.album_table, self.songs_table]:
             table.verticalHeader().hide()
             table.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
+            table.setSelectionMode(QtWidgets.QAbstractItemView.SingleSelection)
             table.setSortingEnabled(True)
             table.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
 
@@ -47,11 +74,12 @@ class Media(QtWidgets.QWidget):
         self.body.addWidget(divide)
         self.body.addWidget(self.songs_table)
 
-        #Why did I do this?
-        self.frame = QtWidgets.QFrame(self)
         self.frame.setLayout(self.body)
 
+        self.setCentralWidget(self.frame)
 
         self.setWindowTitle("Media Library")
+        self.setWindowFlags(self.windowFlags() & Qt.CustomizeWindowHint)
+        self.setWindowFlags(self.windowFlags() & ~Qt.WindowMinMaxButtonsHint)
 
 
