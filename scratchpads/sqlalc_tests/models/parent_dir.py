b33e27d4c087cae3f35779cf1102856b33833f24
devdave <devdave@ominian.net>
2021-05-18 06:58:44 -0600

    Add a GetCreate guard helper and a unique constraint

--- scratchpads/sqlalc_tests/models/parent_dir.py
+++ scratchpads/sqlalc_tests/models/parent_dir.py
@@ -1,8 +1,20 @@
+from pathlib import Path
 
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from ..lib.path_type import PathType
 
 from .base import Base
 
 class ParentDir(Base):
-    path = Column(String)
+    path = Column(PathType(), unique=True)
+
+    @classmethod
+    def GetCreate(cls, dir_path: Path):
+
+        record = cls.query.filter(cls.path == dir_path).first()
+
+        if record is None:
+            record = cls(path=dir_path)
+
+        return record
