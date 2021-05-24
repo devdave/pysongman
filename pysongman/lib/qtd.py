9f6b5891feb5a85c42d6bf909a5b0b313eda4753
devdave <devdave@ominian.net>
2021-05-24 17:37:04 -0600

    Expose QLabel, QFileDialog, and QObject as they're the most common components used in project

--- pysongman/lib/qtd.py
+++ pysongman/lib/qtd.py
@@ -1,7 +1,8 @@
 try:
-    from PySide2.QtWidgets import QApplication
+    from PySide2.QtWidgets import QApplication, QFileDialog, QLabel
     from PySide2 import QtCore
-    from PySide2.QtCore import Qt, Signal, Slot
+    from PySide2.QtCore import Qt, Signal, Slot, QObject
     from PySide2 import QtGui, QtWidgets
+
 except ImportError:
     raise NotImplementedError("Missing PyQT5")
