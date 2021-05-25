5789a9a360055f1436e9ee649cf53eba4d287aea
devdave <devdave@ominian.net>
2021-05-25 13:04:08 -0600

    Gut in favor of MDI library

---
+++ pysongman/controllers/library/audio.py
@@ -0,0 +1,34 @@
+from pysongman.lib.qtd import QtCore, Qt, QObject
+from pysongman.views.library.audio import AudioWindow
+from pysongman.tables.album import AlbumTable
+from pysongman.tables.artist import ArtistTable
+
+
+class AudioLibraryController(QObject):
+
+    artist_table: ArtistTable
+    album_table: AlbumTable
+
+    def __init__(self):
+        super(AudioLibraryController, self).__init__()
+        self.view = AudioWindow()
+
+
+    def setup_models(self):
+        self.artist_table = ArtistTable(
+            {
+                "name": dict(fetcher=lambda r: r.name),
+                "Albums": dict(fetcher=lambda r: r.albums.count()),
+                "Songs": dict(fetcher=lambda r: len(r.artists))
+
+            }
+        )
+        self.album_table = AlbumTable(
+            {
+                "name": dict(fetcher=lambda r: r.name),
+                "songs": dict(fetcher=lambda r: len(r.songs))
+            }
+        )
+
+        self.view.artist_table.setModel(self.artist_table)
+        self.view.album_table.setModel(self.album_table)
--- pysongman/controllers/media.py
+++ pysongman/controllers/media.py
@@ -1,63 +1,25 @@
-from .. import USE_PYSIDE
-
-
-from ..lib.qtd import QtCore, Signal
+import logging
+from ..lib.qtd import QtCore, Signal, QtWidgets, Qt
 
 from pybass3.pys2_playlist import Pys2Playlist
 
+from ..lib.mdi_controller import MDIController
 from ..views.media_window import MediaWindow
-
+from ..controllers.library.audio import AudioLibraryController
 from ..tables.artist import ArtistTable
 from ..tables.album import AlbumTable
 
-
-class MediaControllerSignals(QtCore.QObject):
-    show_config = Signal()
+log = logging.getLogger(__name__)
 
 
-class MediaController(QtCore.QObject):
+class MediaController(MDIController):
 
-    signals: MediaControllerSignals
-
-    def __init__(self, playlist: Pys2Playlist):
+    DEFAULT_VIEW = MediaWindow
+    
+    def __init__(self):
         super(MediaController, self).__init__()
-        self.signals = MediaControllerSignals()
 
-        self.playlist = playlist
-        self.view = MediaWindow()
-
-        self.setup_connections()
-        self.setup_models()
-
-    def setup_connections(self):
-        self.view.act_lib_config.triggered.connect(lambda : self.signals.show_config.emit())
-        pass
-
-    def setup_models(self):
-        self.artist_table = ArtistTable(
-            {
-                "name": dict(fetcher=lambda r: r.name),
-                "Albums": dict(fetcher=lambda r: r.albums.count()),
-                "Songs": dict(fetcher=lambda r: len(r.artists))
-
-            }
-        )
-        self.album_table = AlbumTable(
-            {
-                "name": dict(fetcher=lambda r: r.name),
-                "songs": dict(fetcher=lambda r: len(r.songs))
-            }
-        )
-
-        self.view.artist_table.setModel(self.artist_table)
-        self.view.album_table.setModel(self.album_table)
-
-    def show(self):
-        self.view.show()
-
-    def hide(self):
-        self.view.hide()
-
-    def activate(self):
-        self.view.activateWindow()
+    def setup_subwindows(self):
+        self.add_controller(AudioLibraryController, "local_media", "Audio", show=True)
+        log.debug("Subwindows added")
 
