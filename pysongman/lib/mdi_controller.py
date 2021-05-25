6a1564d0320bc9e55fe1b0fa6ee9bcaf0035934d
devdave <devdave@ominian.net>
2021-05-25 13:02:13 -0600

    Make a common MDI view

---
+++ pysongman/lib/mdi_controller.py
@@ -0,0 +1,99 @@
+import logging
+
+import pysongman
+from pysongman.views.config_window import ConfigWindow
+from pysongman.controllers.config.debug import ConfigDebugController
+
+from pysongman.lib.qtd import QtCore, QtWidgets, Qt
+
+
+log = logging.getLogger(__name__)
+
+
+class MDIControllerSignals(QtCore.QObject):
+    closed = QtCore.Signal()
+
+
+class MDIController(QtCore.QObject):
+
+    DEFAULT_VIEW: ConfigWindow = None
+    signals: MDIControllerSignals
+
+    def __init__(self):
+        super(MDIController, self).__init__()
+
+        self.signals = MDIControllerSignals()
+        self.view = self.DEFAULT_VIEW()
+        self.controllers = {}
+        self.folders = {}
+        self.subviews = {}
+
+        log.debug("%s Config controller initialized", self.__class__.__name__)
+
+        self.setup_connections()
+        self.setup_subwindows()
+
+    def setup_connections(self):
+        self.view.menu_tree.itemClicked.connect(self.on_menu_tree_item_clicked)
+        self.view.close_button.clicked.connect(self.on_close)
+        log.debug("Connections setup")
+
+
+
+    def add_controller(self, controller_cls, identifier, label, group_id = None, show = False):
+        log.debug("Adding controller %s(%s) to %s", label, identifier, group_id)
+
+        if group_id is None:
+            root = self.view.menu_tree
+        elif group_id in self.folders:
+            root = self.folders[group_id]
+        else:
+            raise ValueError(f"{group_id!r} is not a valid folder. options are {self.folders.keys()!r}")
+
+        self.controllers[identifier] = controller_cls()
+
+        self.folders[identifier] = QtWidgets.QTreeWidgetItem(root, [label])
+        self.folders[identifier].setData(2, QtCore.Qt.EditRole, identifier)
+
+        if show is True:
+            self.subviews[identifier] = self.view.mdi.addSubWindow(self.controllers[identifier].view)
+            self.show_subview(self.subviews[identifier])
+
+    def show_subview(self, sub_window: QtWidgets.QWidget):
+        sub_window.setWindowFlag(Qt.FramelessWindowHint, True)
+        sub_window.showMaximized()
+        sub_window.show()
+
+
+    def hide(self):
+        self.view.hide()
+
+    def show(self):
+        self.view.show()
+
+    def activate(self):
+        self.view.activateWindow()
+
+    def on_menu_tree_item_clicked(self, treeItem: QtWidgets.QTreeWidgetItem):
+        label = treeItem.text(0)
+        identifier = treeItem.text(2)
+        log.debug("Config menu %s(%s) was requested", label, identifier)
+
+        controller = self.controllers[identifier]
+
+        if identifier not in self.subviews:
+            sub = self.view.mdi.addSubWindow(controller.view)
+            self.subviews[identifier] = sub
+        else:
+            for subview in self.subviews.values():
+                subview.hide()
+
+            sub = self.subviews[identifier]
+
+        sub.setWindowFlag(Qt.FramelessWindowHint, True)
+        sub.showMaximized()
+        sub.show()
+
+
+    def on_close(self):
+        self.signals.closed.emit()
---
+++ pysongman/lib/mdi_window.py
@@ -0,0 +1,41 @@
+
+from .qtd import QtCore, QtWidgets, Qt
+
+class MDIWindowSignals(QtCore.QObject):
+    pass
+
+class MDIWindow(QtWidgets.QMainWindow):
+
+    menu_tree: QtWidgets.QTreeWidget
+    mdi: QtWidgets.QMdiArea
+    signals: MDIWindowSignals
+    close_button: QtWidgets.QPushButton
+
+    def __init__(self):
+        super(MDIWindow, self).__init__()
+        self.signals = MDIWindowSignals()
+
+        self.setup_ui()
+
+
+    def setup_ui(self):
+        divider = QtWidgets.QSplitter(Qt.Horizontal, self)
+        menu_side = QtWidgets.QVBoxLayout(self)
+        menu_frame = QtWidgets.QFrame()
+
+        self.menu_tree = QtWidgets.QTreeWidget(self)
+        self.close_button = QtWidgets.QPushButton("Close")
+
+        menu_side.addWidget(self.menu_tree)
+        menu_side.addWidget(self.close_button)
+        menu_frame.setLayout(menu_side)
+
+
+        self.mdi = QtWidgets.QMdiArea(self)
+
+
+
+        divider.addWidget(menu_frame)
+        divider.addWidget(self.mdi)
+
+        self.setCentralWidget(divider)
