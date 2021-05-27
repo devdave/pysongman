18f07c469a0826fd43b6cc2f2b3eb29aaeee2494
devdave <devdave@ominian.net>
2021-05-27 15:15:50 -0600

    Additional objects from pyside2 exposed directly

--- pysongman/lib/qtd.py
+++ pysongman/lib/qtd.py
@@ -1,6 +1,6 @@
 try:
     from PySide2.QtWidgets import QApplication, QFileDialog, QLabel, QHBoxLayout, QVBoxLayout
-    from PySide2.QtWidgets import QLabel, QPushButton, QLineEdit, QFrame
+    from PySide2.QtWidgets import QLabel, QPushButton, QLineEdit, QFrame, QMenu, QAction
     from PySide2 import QtCore
     from PySide2.QtCore import Qt, Signal, Slot, QObject
     from PySide2 import QtGui, QtWidgets
