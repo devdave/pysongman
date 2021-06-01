1f68bbfde09710512cd5e244a9719b9a1e775099
devdave <devdave@ominian.net>
2021-06-01 07:26:02 -0600

    Additional explicit imports

--- pysongman/lib/qtd.py
+++ pysongman/lib/qtd.py
@@ -4,6 +4,10 @@ try:
     from PySide2 import QtCore
     from PySide2.QtCore import Qt, Signal, Slot, QObject
     from PySide2 import QtGui, QtWidgets
+    import PySide2
+    from PySide2.QtGui import QPixmap
+
+    from PySide2.QtWidgets import QAction, QHBoxLayout, QLabel, QPushButton, QVBoxLayout
 
 except ImportError:
     raise NotImplementedError("Missing PyQT5")
