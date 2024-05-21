e2c41d6f3f33da36546833cb8d363aa497fdccef
devdave <devdave@ominian.net>
2024-05-20 20:02:22 -0600

    Just testing the api

--- ui/src/pages/Home.page.tsx
+++ ui/src/pages/Home.page.tsx
@@ -1,6 +1,13 @@
 import { Text } from '@mantine/core'
+import { useAppContext } from '@src/contexts/App.context';
 
 export function HomePage() {
+    const { api } = useAppContext()
+
+    api.songs.list(1, 100).then((response) => {
+        console.log(response)
+    })
+
   return (
     <>
       <Text>PySongMan</Text>
